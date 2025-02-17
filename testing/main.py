595f8b826100ddb61778a67694e0368bb46d64b8
Sai Gowtham <78816361+gowthamgdn@users.noreply.github.com>
2025-02-17 08:47:16 +0530

    Update main.py

--- testing/main.py
+++ testing/main.py
@@ -1,5 +1,1 @@
-from quote_generator import get_quote
-
-# Call the function and print the motivational quote
-print("Testing quote_generator package:")
-print(get_quote())
+import gowtham_quote_generator
